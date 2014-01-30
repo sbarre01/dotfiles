907c80ba10ab6b435700e97663145eb8f45b7057
Jim Lawton <jim.lawton@s3group.com>
2014-01-30 16:41:28 +0000

    First pass at making a bash setup script.

--- _setupdotfiles.sh
+++ _setupdotfiles.sh
@@ -1,42 +1,40 @@
-export SHELLSCONFIGDIR=.dotfiles
+export SHELLSCONFIGDIR=~/dotfiles
 
 pushd ~
 
-# Unlink existing
-.bash_history
-.bash_profile
-.bashrc
-.conf
-.gitconfig
-.gitignore
-.netrc
-.profile
-.rvmrc
-.shellaliases
-.shellactivities
-.shellpaths
-.shellvars
-.vimrc
+DOTFILES=".bash_history .bash_profile .bashrc .gitconfig .gitignore .netrc .profile .vimrc"
+DOTDIRS=".vim"
 
-# Link
-.bash_history
-.bash_profile
-.bashrc
-.conf
-.gitconfig
-.gitignore
-.netrc
-.profile
-.rvmrc
-.shellactivities
-.shellaliases
-.shellpaths
-.shellvars
-.vimrc
+if [ -d ~/.old ]; then
+	echo "~/.old already exists! Please clean up and try again."
+	exit 1
+fi
+
+mkdir ~/.old
+
+for dotfile in $DOTFILES; do
+	filename=~/$dotfile
+	if [ -f $filename ]; then
+		echo "Found existing dotfile $filename!"
+		echo "mv $filename ~/.old/"
+	fi
+	sourcefile=$(echo $dotfile | sed 's/^\.//')
+	echo "ln -s $SHELLSCONFIGDIR/$sourcefile ~/"
+done
+
+for dotdir in $DOTDIRS; do
+	dirname=~/$dotdir
+	if [ -d $dirname ]; then
+		echo "Found existing directory $dirname!"
+		echo "mv $dirname ~/.old/"
+	fi
+	sourcedir=$(echo $dotdir | sed 's/^\.//')
+	echo "ln -s $SHELLSCONFIGDIR/$sourcedir ~/"
+done
 
 
 # Cleanup
-rm .zsh_history && ln -s $SHELLSCONFIGDIR/.zsh_history
+#rm .zsh_history && ln -s $SHELLSCONFIGDIR/.zsh_history
 
 popd
 
