bbed3cbc73c389d02aceec3d7558a13d6bc01d39
Jim Lawton <jim.lawton@gmail.com>
2014-02-01 12:53:54 +0000

    Add .inputrc.

--- _setupdotfiles.sh
+++ _setupdotfiles.sh
@@ -1,6 +1,6 @@
 SHELLSCONFIGDIR=~/dotfiles
 DOTFILESDIR=~/dotfiles
-DOTFILES=".bash_logout .bash_profile .bashrc .colordiffrc .colorgccrc .cshrc .gitconfig .gitignore .profile .vimrc .vim .xxdiffrc"
+DOTFILES=".bash_logout .bash_profile .bashrc .colordiffrc .colorgccrc .cshrc .gitconfig .gitignore .inputrc .profile .vimrc .vim .xxdiffrc"
 MOVE=true
 SAVEDIR=~/.old
 
