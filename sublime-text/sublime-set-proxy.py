292cec5b091d276f3f3d9fe504eff7410042d585
Jim Lawton <jim.lawton@gmail.com>
2014-04-26 17:10:56 +0100

    Fix script.

--- sublime-text/sublime-set-proxy.py
+++ sublime-text/sublime-set-proxy.py
@@ -2,13 +2,14 @@
 # Python script to set/unset SublimeText Package Control proxy setting..
 # Jim Lawton, 26th April, 2014.
 
+from __future__ import print_function
 import os
 import os.path
 import sys
 from optparse import OptionParser
 import json
 
-_CFGFILE = "~/.config/sublime-text-3/Packages/User/Package\ Control.sublime-settings"
+_CFGFILE = ".config/sublime-text-3/Packages/User/Package Control.sublime-settings"
 
 
 def _getOpts():
@@ -19,7 +20,7 @@ def _getOpts():
                           "master configuration file.")
     (opts, args) = parser.parse_args()
     opts.proxy = None
-    if len(args) < 1:
+    if len(args) > 0:
         opts.proxy = args[0]
     return (opts, args)
 
@@ -28,22 +29,26 @@ def main():
     "Main function."
     opts, args = _getOpts()
 
+    cfgfile = os.path.join(os.getenv("HOME"), _CFGFILE)
     config = {}
-    if os.path.exists(_CFGFILE):
-        with open(_CFGFILE, 'r') as f:
+    if os.path.exists(cfgfile):
+        with open(cfgfile, 'r') as f:
             config = json.load(f)
 
     if "http_proxy" in config.keys():
         if opts.proxy:
+            print("Setting proxy to %s" % opts.proxy)
             config["http_proxy"] = opts.proxy
         else:
+            print("Clearing proxy setting")
             del config["http_proxy"]
     else:
         if opts.proxy:
+            print("Setting proxy to %s" % opts.proxy)
             config["http_proxy"] = opts.proxy
 
-    with open(_CFGFILE, 'w') as f:
-        json.dump(config, f, indent=None, sort_keys=True)
+    with open(cfgfile, 'w') as f:
+        json.dump(config, f, indent=8, sort_keys=True)
 
 if __name__ == '__main__':
     main()
