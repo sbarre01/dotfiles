7b16e4b8a6aca54355368c8e1fb55ef95e0b0b51
Jim Lawton <jim.lawton@s3group.com>
2014-12-09 15:02:52 +0000

    Fix description. Handle non-existent config file.

--- sublime-text/sublime-set-proxy.py
+++ sublime-text/sublime-set-proxy.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Python script to set/unset SublimeText Package Control proxy setting..
+# Python script to set/unset SublimeText Package Control proxy setting.
 # Jim Lawton, 26th April, 2014.
 
 from __future__ import print_function
@@ -16,8 +16,8 @@ def _getOpts():
     """Parse command line options and arguments."""
     usage = "%prog [options] [proxy-url]"
     parser = OptionParser(usage=usage,
-                          description="Update the CMF "
-                          "master configuration file.")
+                          description="Set/unset SublimeText Package Control "
+                          "proxy setting.")
     (opts, args) = parser.parse_args()
     opts.proxy = None
     if len(args) > 0:
@@ -35,20 +35,20 @@ def main():
         with open(cfgfile, 'r') as f:
             config = json.load(f)
 
-    if "http_proxy" in config.keys():
-        if opts.proxy:
-            #print("Setting SublimeText proxy to %s" % opts.proxy)
-            config["http_proxy"] = opts.proxy
+        if "http_proxy" in config.keys():
+            if opts.proxy:
+                #print("Setting SublimeText proxy to %s" % opts.proxy)
+                config["http_proxy"] = opts.proxy
+            else:
+                #print("Clearing SublimeText proxy setting")
+                del config["http_proxy"]
         else:
-            #print("Clearing SublimeText proxy setting")
-            del config["http_proxy"]
-    else:
-        if opts.proxy:
-            #print("Setting SublimeText proxy to %s" % opts.proxy)
-            config["http_proxy"] = opts.proxy
-
-    with open(cfgfile, 'w') as f:
-        json.dump(config, f, indent=8, sort_keys=True)
+            if opts.proxy:
+                #print("Setting SublimeText proxy to %s" % opts.proxy)
+                config["http_proxy"] = opts.proxy
+    
+        with open(cfgfile, 'w') as f:
+            json.dump(config, f, indent=8, sort_keys=True)
 
 
 if __name__ == '__main__':
