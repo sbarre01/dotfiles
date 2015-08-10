b88ae5095d59ab9e32549bdb5b653019bd57c197
Jim Lawton <jim.lawton@gmail.com>
2015-08-10 19:09:30 +0100

    Return rather than exit if .apmrc not found.

--- atom/atom-set-proxy.py
+++ atom/atom-set-proxy.py
@@ -32,7 +32,8 @@ def main():
     for cfgfile in [_APM_CFGFILE, _NPM_CFGFILE]:
         cfgfile = os.path.join(os.getenv("HOME"), cfgfile)
         if not os.path.exists(cfgfile):
-            sys.exit("Config file %s does not exist!" % cfgfile)
+            print("Config file %s does not exist!" % cfgfile)
+            return
 
         lines = newlines = []
         with open(cfgfile, 'r') as f:
